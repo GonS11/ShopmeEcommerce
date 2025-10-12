8342ae28812a244e01000e1b48c79ab2b2fdafd0
“Gonzalo” <gonzalos.camara@gmail.com>
2025-10-12 12:59:27 +0200

    User repository + General configuration backend + Photos

--- ShopmeWebParent/ShopmeBackend/src/main/java/com/shopme/admin/user/UserRepository.java
+++ ShopmeWebParent/ShopmeBackend/src/main/java/com/shopme/admin/user/UserRepository.java
@@ -6,20 +6,21 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
-public interface UserRepository extends SearchRepository<User, Integer>
-{
-    @Query("SELECT u FROM User u WHERE u.email = :email")
-    public User getUserByEmail(@Param("email") String email);
+@Repository
+public interface UserRepository extends SearchRepository<User, Integer> {
 
-    public Long countById(Integer id);
+    User findByEmail(String email);
 
-    @Query("SELECT u FROM User u WHERE CONCAT(u.id, ' ', u.email, ' ', u.firstName, ' ',"
-            + " u.lastName) LIKE %?1%")
-    public Page<User> findAll(String keyword, Pageable pageable);
+    Long countById(Integer id);
 
-    @Query("UPDATE User u SET u.enabled = ?2 WHERE u.id = ?1")
+    @Query("SELECT u FROM User u WHERE CONCAT(u.id, ' ', u.email, ' ', u.firstName, ' ', u.lastName) LIKE %?1%")
+    Page<User> search(String keyword, Pageable pageable);
+
+    @Transactional
     @Modifying
-    public void updateEnabledStatus(Integer id, boolean enabled);
+    @Query("UPDATE User u SET u.enabled = ?2 WHERE u.id = ?1")
+    void updateEnabledStatus(Integer id, boolean enabled);
 }
