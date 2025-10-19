e204fa13fec6e412bcb89477a7d87494e2d0893d
“Gonzalo” <gonzalos.camara@gmail.com>
2025-10-19 14:12:21 +0200

    Customer service + Controller

--- ShopmeCommon/src/main/java/com/shopme/common/entity/StateDTO.java
+++ ShopmeCommon/src/main/java/com/shopme/common/entity/StateDTO.java
@@ -8,4 +8,9 @@ public class StateDTO
     /*TODO: Change DTO place*/
     private Integer id;
     private String name;
+
+    public StateDTO(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
 }
